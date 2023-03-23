0c9025a47f2569050a1b1fe87c76a947705dbf2f
JustRio24 <128729221+JustRio24@users.noreply.github.com>
2023-03-24 01:25:28 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,7 +1,7 @@
 function popupku() {
   var body = document.querySelector("body");      
   var pWm = document.createElement("a");
-  pWm.setAttribute("href", "");
+  pWm.setAttribute("href", "https://www.instagram.com/justrio.24/");
   pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
   pWm.innerHTML = "Dari Rio untuk Dea Tersayang";
   body.appendChild(pWm);
