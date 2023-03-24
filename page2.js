0a0d862e233e24e2aa0ea89483647d4ceb2044f4
JustRio24 <128729221+JustRio24@users.noreply.github.com>
2023-03-24 09:30:05 +0700

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,5 +1,5 @@
 var html =
-  '<h2>Pesan Berhasil Terkirim</h2><h3>Anda akan diarahkan ke halaman utama</h3><h4 class="timer">10</h4><p>Dibuat Oleh :</p><a href="https://www.instagram.com/justrio.24/" class="yt"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1024px-Instagram_logo_2016.svg.png" height="30px" alt="" srcset="" />JustRio</a><div class="banner"><p>Happy Legal Day</p><a href="https://www.instagram.com/justrio.24/"><img src="https://justrio24.github.io/justrio.github.io/yay-couple.gif" height="1280px" width="720px" alt="" /></a></div>';
+  '<div class="banner"><p>Happy Legal Day</p><a href="https://www.instagram.com/justrio.24/"><img src="https://justrio24.github.io/justrio.github.io/yay-couple.gif" height="1280px" width="720px" alt="" /></a></div>';
 
 document.querySelector("body").innerHTML += html;
 
